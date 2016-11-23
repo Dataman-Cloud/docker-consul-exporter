e217930fa4a0d6da3d2d65433df8b6e8ce6b687f
pangzheng <pangzheng@homtail.com>
2016-11-23 18:14:17 +0800

    up

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -1,4 +1,4 @@
 base_dir=$(cd `dirname $0` && pwd)
 cd $base_dir
 opts='--no-cache'
-docker build $opts -t library/centos7-consul-exporter-0.2.0 . 
+docker build $opts -t library/centos7-consul-exporter-0.2.0 $base_dir
